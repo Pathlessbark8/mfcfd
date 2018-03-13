74bab5ed89f7220294e56c1259ff00eb68343cec
srikanthcs05 <srikanthcs05@bitbucket.org>
2018-03-13 16:01:47 +0530

    few minor previous edits

--- src_ser/meshfree_solver.f90
+++ src_ser/meshfree_solver.f90
@@ -8,7 +8,7 @@ program meshfree_solver
 	use parameter_mod
 	use data_structure_mod
 	use point_preprocessor_mod
-!    use initial_conditions_mod
+    use initial_conditions_mod
 !	use q_lskum_mod
 !	use post_processing_mod
 !
@@ -22,7 +22,7 @@ program meshfree_solver
 !
 !	Assign the initial conditions for the primitive variables ..	
 !
-!	call initial_conditions()
+	call initial_conditions()
 !
 !	Primal fixed point iterative solver ..
 !
