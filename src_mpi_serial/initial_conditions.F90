fe6c1941d2a60c8b7afff3fb04f7f98b5d87d5fb
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2020-01-22 12:18:10 +0530

    Maybe this is better?

---
+++ src_mpi_serial/initial_conditions.F90
@@ -0,0 +1,62 @@
+module initial_conditions_mod
+
+        use data_structure_mod
+        use parameter_mod
+
+contains
+
+        subroutine initial_conditions()
+
+                implicit none
+
+                integer :: k,i
+
+                if(restart == 0) then
+
+                        call setup_case_parameters()
+
+                        do k=1, max_points
+                                point%prim(1,k) = q_init(1)
+                                point%prim(2,k) = q_init(2)
+                                point%prim(3,k) = q_init(3)
+                                point%prim(4,k) = q_init(4)
+                        enddo
+
+                elseif(restart == 1) then
+
+                        write(*,*)'%%%%%%%%%%%%-Using restart file-%%%%%%%%%%%'
+                        write(*,*)
+
+                        call setup_case_parameters()
+
+                        call restart_sol()
+
+                endif
+
+        end subroutine
+
+        subroutine restart_sol()
+
+                implicit none
+
+                integer :: i, dummy
+                character(len=64) :: sfile
+                character(len=10) :: itos
+
+                sfile = 'restart/sol.dat'
+
+                OPEN(UNIT=515,FILE=trim(sfile),form='formatted', action="read")
+
+                read(515,*)dummy, itr, res_old
+
+                do i = 1, max_points
+                        read(515,*)dummy, dummy, dummy, dummy,&
+                                dummy, point%prim(1,i), point%prim(2,i), point%prim(3,i),&
+                                point%prim(4,i)
+                end do
+
+                close(515)
+
+        end subroutine
+
+end module initial_conditions_mod
