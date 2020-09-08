0bc1953ad1dac10c36bae324c737d53a70c0781b
Testsubjector <prasunk2@gmail.com>
2020-09-08 19:28:21 +0530

    Basic changes

---
+++ examples/parameter_mod.mod
@@ -0,0 +1,60 @@
+GFORTRAN module version '10' created from /home/meshfree/mfcfd/examples/parameter.F90
+MD5:708bf4c19cc7d5de891cdbfd66b395b2 -- If you edit this, you'll get what you deserve.
+
+(() () () () () () () () () () () () () () () () () () () () () () () ()
+() () ())
+
+()
+
+()
+
+()
+
+()
+
+()
+
+(2 'aoa' 'parameter_mod' '' 1 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (REAL 8 0 0 0 REAL ()) 0 0 () () 0 () () () 0
+0)
+3 'datan' '(intrinsic)' '' 1 ((PROCEDURE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN UNKNOWN 0 0 FUNCTION) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () () 3 ()
+() () 0 0)
+4 'gamma' 'parameter_mod' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (REAL 8 0 0 0 REAL ()) 0 0 () (CONSTANT (
+REAL 8 0 0 0 REAL ()) 0 '0.16666666666666@1') () 0 () () () 0 0)
+5 'mach' 'parameter_mod' '' 1 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (REAL 8 0 0 0 REAL ()) 0 0 () () 0 () () () 0
+0)
+6 'parameter_mod' 'parameter_mod' '' 1 ((MODULE UNKNOWN-INTENT
+UNKNOWN-PROC UNKNOWN UNKNOWN 0 0) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () ()
+0 () () () 0 0)
+7 'pi' 'parameter_mod' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (REAL 8 0 0 0 REAL ()) 0 0 () (CONSTANT (
+REAL 8 0 0 0 REAL ()) 0 '0.3243f6a8885a30@1') () 0 () () () 0 0)
+8 'pr_inf' 'parameter_mod' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (REAL 8 0 0 0 REAL ()) 0 0 () (CONSTANT (
+REAL 8 0 0 0 REAL ()) 0 '0.b6db6db6db6db8@0') () 0 () () () 0 0)
+9 'q_inf' 'parameter_mod' '' 1 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0 DIMENSION) (REAL 8 0 0 0 REAL ()) 0 0 () (1 0
+EXPLICIT (CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '1') (CONSTANT (
+INTEGER 4 0 0 0 INTEGER ()) 0 '4')) 0 () () () 0 0)
+10 'q_init' 'parameter_mod' '' 1 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0 DIMENSION) (REAL 8 0 0 0 REAL ()) 0 0 () (1 0
+EXPLICIT (CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '1') (CONSTANT (
+INTEGER 4 0 0 0 INTEGER ()) 0 '4')) 0 () () () 0 0)
+11 'rho_inf' 'parameter_mod' '' 1 ((PARAMETER UNKNOWN-INTENT
+UNKNOWN-PROC UNKNOWN IMPLICIT-SAVE 0 0) (REAL 8 0 0 0 REAL ()) 0 0 () (
+CONSTANT (REAL 8 0 0 0 REAL ()) 0 '0.10000000000000@1') () 0 () () () 0
+0)
+12 'setup_case_parameters' 'parameter_mod' '' 1 ((PROCEDURE
+UNKNOWN-INTENT MODULE-PROC DECL UNKNOWN 0 0 SUBROUTINE) (UNKNOWN 0 0 0 0
+UNKNOWN ()) 0 0 () () 0 () () () 0 0)
+13 'theta' 'parameter_mod' '' 1 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (REAL 8 0 0 0 REAL ()) 0 0 () () 0 () () () 0
+0)
+)
+
+('aoa' 0 2 'datan' 0 3 'gamma' 0 4 'mach' 0 5 'parameter_mod' 0 6 'pi' 0
+7 'pr_inf' 0 8 'q_inf' 0 9 'q_init' 0 10 'rho_inf' 0 11
+'setup_case_parameters' 0 12 'theta' 0 13)
--- src_cuda/meshfree_solver.F90
+++ src_cuda/meshfree_solver.F90
@@ -1,83 +1,115 @@
 program meshfree_solver
-
-        use cudafor
-        use parameter_mod
-        use data_structure_mod
-        use point_preprocessor_mod
-        use initial_conditions_mod
-        use q_lskum_mod
-        use post_processing_mod
-        use objective_function_mod
-        use adaptation_sensors_mod
-
-
-        implicit none
-        integer :: istat, i, nDevices=0
-        real*8  :: start,finish, runtime
-        type(cudaDeviceProp) :: prop
-        
-        call cpu_time(start)
-
+    
+    use cudafor
+    use parameter_mod
+    use data_structure_mod
+    use point_preprocessor_mod
+    use initial_conditions_mod
+    use q_lskum_mod
+    use post_processing_mod
+    use objective_function_mod
+    use adaptation_sensors_mod
+    
+    
+    implicit none
+    integer :: istat, i, j, nDevices=0
+    integer :: accessPeer1, accessPeer2
+    real*8  :: start,finish, runtime
+    type(cudaDeviceProp) :: prop
+    
+    call cpu_time(start)
+    
+    write(*,*)
+    write(*,*)'%%%%%%%%-CUDA Fortran Meshfree Code-%%%%%%%'
+    write(*,*)
+    write(*,*)'%%%%%%%%%%%%%%%-Device info-%%%%%%%%%%%%%%%'
+    istat = cudaGetDeviceCount ( nDevices )
+    do i = 0, nDevices - 1
+        istat = cudaGetDeviceProperties(prop, i)
+        write(*,*)'Device Name:               ', trim(prop%name)
+        write(*,*)'Compute Capability:        ',prop%major, prop%minor
+        write(*,*)'Device number:             ',i
+        write(*,*)'MemoryClockRate(KHz):      ',prop%memoryBusWidth
+        write(*,*)'PeakMemoryBandwidth(GB/s): ',2.0 *prop%memoryClockRate * &
+        & (prop%memoryBusWidth/8) * 1.e-6
         write(*,*)
-        write(*,*)'%%%%%%%%-CUDA Fortran Meshfree Code-%%%%%%%'
-        write(*,*)
-        write(*,*)'%%%%%%%%%%%%%%%-Device info-%%%%%%%%%%%%%%%'
-        istat = cudaGetDeviceCount ( nDevices )
-        do i = 0, nDevices - 1
-                istat = cudaGetDeviceProperties(prop, i)
-                write(*,*)'Device Name:               ', trim(prop%name)
-                write(*,*)'Compute Capability:        ',prop%major, prop%minor
-                write(*,*)'Device number:             ',i
-                write(*,*)'MemoryClockRate(KHz):      ',prop%memoryBusWidth
-                write(*,*)'PeakMemoryBandwidth(GB/s): ',2.0 *prop%memoryClockRate * &
-                        & (prop%memoryBusWidth/8) * 1.e-6
-                write(*,*)
-        end do
-
-!       Set up case input
-        call readnml()
-
-!	Reading the input data ..
-        write(*,*) '%%%%%%%%%%%%-Reading point file-%%%%%%%%%%%'
-        call read_input_point_data()
-        write(*,*) 'Number of points:         ', max_points
-        write(*,*) 'Number of wall points:    ', wall_points
-        write(*,*) 'Number of shape points:   ', shape_points
-        write(*,*) 'Number of interior points:', interior_points
-        write(*,*) 'Number of outer points:   ', outer_points
-        write(*,*)
-
-!       Allocate solution variables
-        call allocate_soln()
-
-!       Allocate device solution variables
-        call allocate_device_soln()
-
-!	Assign the initial conditions for the primitive variables ..	
-        call initial_conditions()
-        write(*,*) '%%%%%%%%%%%-Solution initialized-%%%%%%%%%%'
-        write(*,*)
-       
-!	Primal fixed point iterative solver ..
-        call q_lskum(runtime)
-
-!       Compute sensor values
-        call compute_adapt_sensor()
-
-!       Objective function computation
-        call objective_function()
-
-!       Save solution one last time
-        call print_primal_output()
-
-!       Deallocate point/solution vectors
-        call deallocate_soln()
-        call dealloc_points()
-        call deallocate_device_soln()
-
-        call cpu_time(finish) 
-        write(*,*) '%%%%%%%%%%%-Simulation finished-%%%%%%%%%%%'
-        write(*,*) 'Run time:  ',runtime,'seconds'
-        write(*,*) 'Total time:',finish-start,'seconds'
-
+    end do
+    
+    if (nDevices .lt. 1) then
+        write(*,*) 'ERROR: There are no devices available on this host.  ABORTING.'
+    endif
+    
+    istat = cudaDeviceSetCacheConfig(cudaFuncCachePreferShared)
+    if (istat /= 0) then
+       print *, 'main: error setting cudaFuncAttributePreferredSharedMemoryCarveout',cudaGetErrorString(istat)
+       stop
+    endif
+    
+    ! establish the Peer-to-Peer connections
+    ! do i=1, istat-1
+    !     do j=i+1,istat
+    !         istat = cudaDeviceCanAccessPeer(accessPeer1, devices(i), devices(j))
+    !         istat = cudaDeviceCanAccessPeer(accessPeer2, devices(j), devices(i))
+    !         if (accessPeer1 .eq. 1 .and. accessPeer2 .eq. 1 ) then
+    !             write(*,"('Peer-to-Peer capable between ',i0, ' and ', i0)") &
+    !             devices(i), devices(j)
+    !             ! enable peer access required for copies
+    !             istat = cudaSetDevice(devices(i))  
+    !             istat = cudaDeviceEnablePeerAccess(devices(j), 0) 
+    !             istat = cudaSetDevice(devices(j))
+    !             istat = cudaDeviceEnablePeerAccess(devices(i), 0)  
+    !         else
+    !             write(*,"('Peer-to-Peer not capable between ',i0, ' and ', i0)") &
+    !             devices(i), devices(j)
+    !             stop
+    !         endif
+    !     enddo
+    ! enddo
+    
+    !       Set up case input
+    call readnml()
+    
+    !	Reading the input data ..
+    write(*,*) '%%%%%%%%%%%%-Reading point file-%%%%%%%%%%%'
+    call read_input_point_data()
+    write(*,*) 'Number of points:         ', max_points
+    write(*,*) 'Number of wall points:    ', wall_points
+    write(*,*) 'Number of shape points:   ', shape_points
+    write(*,*) 'Number of interior points:', interior_points
+    write(*,*) 'Number of outer points:   ', outer_points
+    write(*,*)
+    
+    !       Allocate solution variables
+    call allocate_soln()
+    
+    !       Allocate device solution variables
+    call allocate_device_soln()
+    
+    !	Assign the initial conditions for the primitive variables ..	
+    call initial_conditions()
+    write(*,*) '%%%%%%%%%%%-Solution initialized-%%%%%%%%%%'
+    write(*,*)
+    
+    !	Primal fixed point iterative solver ..
+    call q_lskum(runtime)
+    
+    !       Compute sensor values
+    call compute_adapt_sensor()
+    
+    !       Objective function computation
+    call objective_function()
+    
+    !       Save solution one last time
+    call print_primal_output()
+    
+    !       Deallocate point/solution vectors
+    call deallocate_soln()
+    call dealloc_points()
+    call deallocate_device_soln()
+    
+    call cpu_time(finish) 
+    write(*,*) '%%%%%%%%%%%-Simulation finished-%%%%%%%%%%%'
+    write(*,*) 'Run time:  ',runtime,'seconds'
+    write(*,*) 'Total time:',finish-start,'seconds'
+    
 end program meshfree_solver
